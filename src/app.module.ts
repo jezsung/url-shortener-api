a4eb3dfe15c27159549386dd210eb549be001fb1
Jaesung Hong <jezsung@gmail.com>
2023-02-04 16:26:50 +0900

    remove not used code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ArticlesModule } from './articles/articles.module';
 import { LinksModule } from './links/links.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, LinksModule],
+  imports: [PrismaModule, LinksModule],
   controllers: [AppController],
   providers: [AppService],
 })
