6157ee4553af3eb03570c337b99e8ba30792d28f
Jaesung Hong <jezsung@gmail.com>
2023-02-04 15:32:50 +0900

    add redirect endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get(':hashedId')
+  async redirectToOriginUrl(
+    @Param('hashedId') hashedId: string,
+    @Res() res: Response,
+  ) {
+    const url = await this.appService.getOriginUrl(hashedId);
+    res.status(301).redirect(url);
+  }
 
   @Get()
   getHello(): string {
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import Hashids from 'hashids/cjs/hashids';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
+
+  async getOriginUrl(hashedId: string): Promise<string> {
+    const hasher = new Hashids();
+    const id = hasher.decode(hashedId);
+    const link = await this.prisma.link.findUnique({ where: { id: +id } });
+    return link.url;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
