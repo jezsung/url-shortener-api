3992d0995748b73ee789fc44dabdf711d7964639
Jaesung Hong <jezsung@gmail.com>
2023-02-06 22:50:31 +0900

    use env for port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LinksModule } from './links/links.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, LinksModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    LinksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
