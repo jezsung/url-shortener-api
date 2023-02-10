63998661743acedc526d213e42f1c872fc46ee3c
Jaesung Hong <jezsung@gmail.com>
2023-02-10 23:02:46 +0900

    return https url on production

--- src/links/links.controller.ts
+++ src/links/links.controller.ts
@@ -1,20 +1,28 @@
 import { Body, Controller, Headers, Post } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { CreateLinkDto } from './dto/create-link.dto';
 import { LinksService } from './links.service';
 
 @Controller('links')
 export class LinksController {
-  constructor(private readonly linksService: LinksService) {}
+  constructor(
+    private readonly linksService: LinksService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post()
   async create(
     @Headers('host') host: string,
     @Body() createLinkDto: CreateLinkDto,
   ) {
+    const isProd =
+      this.configService.get<'development' | 'production'>('NODE_ENV') ===
+      'production';
+
     const hashedId = await this.linksService.create(createLinkDto);
 
     return {
-      shortenedUrl: `http://${host}/${hashedId}`,
+      shortenedUrl: `http${isProd ? 's' : ''}://${host}/${hashedId}`,
     };
   }
 }
